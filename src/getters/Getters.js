8530fe5a835d7153a15ddc622166e5f4ca070144
VytautasStankevicius <v.stankeviciuss@gmail.com>
2024-01-06 23:21:33 +0200

    v4

--- src/getters/Getters.js
+++ src/getters/Getters.js
@@ -16,34 +16,34 @@ export function randomTimer(min, max) {
 
 
 //Kategorijos 
-export const Category = () => {
-  const [category, setCategory] = useState([]);
+// export const Category = () => {
+//   const [category, setCategory] = useState([]);
 
-  useEffect(() => {
-    try {
-      fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-        .then(response => response.json())
-        .then(data => setCategory(data.categories));
-    } catch (msg) {
-      console.log(`category fetch:${msg}`);
-    }
-  }, []);
+//   useEffect(() => {
+//     try {
+//       fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+//         .then(response => response.json())
+//         .then(data => setCategory(data.categories));
+//     } catch (msg) {
+//       console.log(`category fetch:${msg}`);
+//     }
+//   }, []);
 
-  return (
-    <Container>
-      <Row className='recipeCategory'>
-        {category.map(categoryItem => (
-          <Col key={categoryItem.idCategory} sm={12} lg={4} className='recipeCategory__items mb-3'>
-            <Link to={`/category/${categoryItem.strCategory}`}>
-              <img src={categoryItem.strCategoryThumb} alt={categoryItem.strCategory} />
-              <h3>{categoryItem.strCategory}</h3>
-            </Link>
-          </Col>
-        ))}
-      </Row>
-    </Container>
-  );
-}
+//   return (
+//     <Container>
+//       <Row className='recipeCategory'>
+//         {category.map(categoryItem => (
+//           <Col key={categoryItem.idCategory} sm={12} lg={4} className='recipeCategory__items mb-3'>
+//             <Link to={`/category/${categoryItem.strCategory}`}>
+//               <img src={categoryItem.strCategoryThumb} alt={categoryItem.strCategory} />
+//               <h3>{categoryItem.strCategory}</h3>
+//             </Link>
+//           </Col>
+//         ))}
+//       </Row>
+//     </Container>
+//   );
+// }
 
 //Recepto puslapis su info
 // export const Recipe = () => {
--- src/modules/RecipeCategory/RecipeCategory.jsx
+++ src/modules/RecipeCategory/RecipeCategory.jsx
@@ -1,9 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Col, Container, Row } from 'react-bootstrap';
-import { Category } from '../../getters/Getters';
+// import { Category } from '../../getters/Getters';
 
-Category()
+export const RecipeCategory = () => {
+  const [category, setCategory] = useState([]);
+
+  useEffect(() => {
+    try {
+      fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+        .then(response => response.json())
+        .then(data => setCategory(data.categories));
+    } catch (msg) {
+      console.log(`category fetch:${msg}`);
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Row className='recipeCategory'>
+        {category.map(categoryItem => (
+          <Col key={categoryItem.idCategory} sm={12} lg={4} className='recipeCategory__items mb-3'>
+            <Link to={`/category/${categoryItem.strCategory}`}>
+              <img src={categoryItem.strCategoryThumb} alt={categoryItem.strCategory} />
+              <h3>{categoryItem.strCategory}</h3>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+}
 
 
 
